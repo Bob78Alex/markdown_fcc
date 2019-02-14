9c8de846759891e4c5433aac5d61f6ecdd7c161f
Alex Babenko <bob78alex@gmail.com>
2019-02-13 19:26:43 -0500

    adding dangerouslyInnerHTML

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {FormGroup, FormLabel, FormControl} from 'react-bootstrap'
+
+
+let marked = require("marked");
 
 class App extends Component {
+
+  state = {
+    markdown: ""
+  }
+
+updateMarkdown = function(markdown){
+  this.setState({markdown});
+}
+
   render() {
+let {markdown} = this.state;
+console.log(markdown);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container">
+      <div>
+      <FormGroup controlId="formControlsTextarea">
+  <ControlLabel>Markdown Input</ControlLabel>
+  <FormControl componentClass="textarea" placeholder="Enter Markdown" value=(markdown) onChange={(event)=>this.updateMarkdown(event.target.value)}> </FormControl>
+</FormGroup>
+        </div>
+        <div>
+        <h1>Markdown output</h1>
+        <div> {{marked(markdown)}}</div>
+        <div dangerouslySetInnerHTML = {{__html: marked(markdown)}}></div>
+        </div>
       </div>
     );
   }
